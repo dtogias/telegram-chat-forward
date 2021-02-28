0156e0f586d736b01addcaabc965dfb8679f5350
Dimitris Togias <dtogias@gmail.com>
2021-02-28 14:53:21 +0200

    chore: fix formatting warnings

--- forwarder.py
+++ forwarder.py
@@ -13,16 +13,19 @@ from settings import API_ID, API_HASH, REPLACEMENTS, forwards, get_forward, upda
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
+
 def intify(string):
     try:
         return int(string)
     except:
         return string
 
+
 def replace(message):
     for old,new in REPLACEMENTS.items():
         message.text = str(message.text).replace(old,new)
     return message
+
 
 async def forward_job():
     ''' the function that does the job 😂 '''
@@ -33,14 +36,11 @@ async def forward_job():
 
     async with TelegramClient(session, API_ID, API_HASH) as client:
 
-        error_occured = False
         for forward in forwards:
             from_chat, to_chat, offset = get_forward(forward)
 
             if not offset:
                 offset = 0
-
-            last_id = 0
 
             async for message in client.iter_messages(intify(from_chat), reverse=True, offset_id=offset):
                 if isinstance(message, MessageService):
@@ -55,10 +55,10 @@ async def forward_job():
                     await asyncio.sleep(delay=fwe.seconds)
                 except Exception as err:
                     logging.exception(err)
-                    error_occured = True
                     break
 
             logging.info('Completed working with %s', forward)
+
 
 if __name__ == "__main__":
     assert forwards
